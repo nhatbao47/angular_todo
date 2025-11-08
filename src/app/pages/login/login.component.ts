fc14b4a1486d67a148f22d89a3888a2ba0875d51
Dung Nguyen <nhatbao@outlook.com>
2025-11-09 06:38:51 +0700

    Reactive form

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,25 +1,28 @@
 import { Component } from '@angular/core';
-import { UserModel } from '../../models/user.model';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'login-page',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  model: UserModel = new UserModel('', '');
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
   loginError = false;
 
   constructor(private authService: AuthService, private route: Router) {}
 
   onSubmit() {
-    if (this.authService.login(this.model.email, this.model.password )) {
+    let user = this.loginForm.getRawValue();
+    if (this.authService.login(user.email ?? '', user.password ?? '')) {
       this.route.navigate(['/todos']);
     } else {
       this.loginError = true;
